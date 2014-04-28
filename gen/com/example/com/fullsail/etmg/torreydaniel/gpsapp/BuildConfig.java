a022d7b7b0553e3c5fd9d2bb077f11a30ddfbf64
TorreyD <torreyd24@fullsail.edu>
2014-04-27 20:07:57 -0400

    GpsTester

---
+++ gen/com/example/com/fullsail/etmg/torreydaniel/gpsapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.com.fullsail.etmg.torreydaniel.gpsapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
